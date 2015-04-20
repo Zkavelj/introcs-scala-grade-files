840a22ccd90a101910933ddc08f2a699181ea154
Zvonko <zkavelj@luc.edu>
2015-04-20 15:31:17 +0000

    hw 2/3 done

--- gradefiles.scala
+++ gradefiles.scala
@@ -47,22 +47,182 @@ object gradefiles extends App {
     (columns, headerNames, quantitiesArray, weightsArray)
   }
 
-  val exampleHeading = "Exams, Homework, Project"
-  val exampleData = "10, a, 25, 35.6"
-
-  parseCSVHeader(exampleHeading) foreach println
-  parseCSVRowOfDoubles(exampleData, -1.0) foreach println
-
-  val results = readCategoryFile("comp150")
-  results match {
-    case (n, h, q, w) => {
-      println("There are $n columns of data")
-      println("Headings")
-      h foreach println
-      println("Quantities")
-      q foreach println
-      println("Weights")
-      w foreach println
-   }
- }
-}
+    
+ def readCourseStudents(courseName:String):(Array[String],Array[String])={//function
+     val courseFileName = s"students_$courseName.txt"
+     val file = Source.fromFile(courseFileName)
+     val lines = file.getLines
+     var IDArray = new Array[String](10)
+     var IDIterator=0
+     var lastNameArray= new Array[String](10)
+     var lastIterator=0
+     var firstNameArray= new Array[String](10)
+     var firstIterator=0
+     var zkavelj="TBD"
+     while(zkavelj=="TBD"){
+         if(lines.hasNext){
+             var temp=lines.next
+             var tempArray=parseCSVHeader(temp)
+             for(a<-0 until tempArray.length){ 
+                 if(a==0){
+                    IDArray(IDIterator)=tempArray(a)
+                    IDIterator+=1
+                 }              
+                  else if(a==1){
+                      lastNameArray(lastIterator)=tempArray(a)
+                      lastIterator+=1
+                 }
+                  else if(a==2){ 
+                      firstNameArray(firstIterator)=tempArray(a)
+                  firstIterator+=1
+                      
+                 }
+             }   
+         }
+          else{ 
+               zkavelj="Zvonko Kavelj"
+         }    
+                (IDArray,lastNameArray,firstNameArray)
+     }
+    def readIndividualScores(ID:String,courseName:String):(Array[String],Array[Int],Array[Int]) = {
+        
+    }             
+    //function to read the coursework information of individual students_$courseName
+     val courseFileName = s"$ID$courseName.data"
+     val file = Source.fromFile(c)
+     var sum=0.0
+     var totalHomeworkScore=0.0
+     var totalExamScore=0.0
+     var totalProjectScore=0.0
+     var totalLabScore=0.0
+     var totalParticipationScore=0.0
+     var homeworkIndex=0
+     var examIndex=0 
+     var projectIndex=0 
+     var labIndex=0 
+     var participationIndex=0 
+     var grade=0.0  
+     var totalWeight=0
+     for(anything<-0 to headerNames.length-1){
+         if(headerNames(anything)=="Homework"){
+           homeworkIndex=anything
+         }
+         else if(headerNames(anything)=="Exams"){
+             examIndex=anything
+         }
+         else if(headerNames(anything)=="Project"){
+             projectIndex=anything
+         }
+         else if(headerNames(anything)=="Labs"){
+             labIndex=anything
+         }
+         else if(headerNames(anything)=="Class Participation"){
+             participationIndex=anything
+         }
+     }
+      if(homeworkArray(0)!=null){
+          for(a<-0 to homeworkArray.length-1){
+              if(homeworkArray(a)!=null){
+           sum+=homeworkArray(a).toInt       
+          }
+      }       
+       totalHomeworkScore=((sum/quantitiesArray(homeworkIndex).toDouble)*weightsArray).toDouble
+       sum=0
+      } 
+      if(examArray(0)!=null){
+          for(b<-0 to examArray.length-1){
+              if(examArray(b)!=null){
+           sum+=examArray(b).toInt   
+           }
+       }
+        totalExamScore=((sum/quantitiesArray(examIndex).toDouble)*weightsArray).toDouble
+        sum=0
+       }
+     if(projectArray(0)!=null){
+          for(c<-0 to examArray.length-1){
+              if(projectArray(c)!=null){
+           sum+=projectArray(c).toInt    
+           }
+        }
+          totalProjectScore=((sum/quantitiesArray(projectIndex).toDouble)*weightsArray).toDouble
+              sum=0    
+         }
+         if(labArray(0)!=null){
+          for(d<-0 to labArray.length-1){
+              if(labArray(d)!=null){
+           sum+=labArray(d).toInt 
+           }    
+       }
+           totalLabScore=((sum/quantitiesArray(labIndex).toDouble)*weightsArray).toDouble
+              sum=0   
+          }
+           if(paricipationArray(0)!=null){
+          for(e<-0 to participationArray.length-1){
+              if(participationArray(e)!=null){
+           sum+=participationArray(e).toInt   
+         }
+       } 
+        totalParticipationScore=((sum/quantitiesArray(participationIndex).toDouble)*weightsArray).toDouble
+              sum=0   
+  }
+     grade=grade+totalHomeworkScore+totalExamScore+totalLabScore+totalProjectScore+totalParticipationScore
+     for(x<-0 to weightsArray.length-1){
+         if(weightsArray(x).toString!=null){
+             totalWeight+=weightsArray(x)
+         }
+     }       
+     grade=(grade/totalWeight)*100
+     grade=BigDecimal(grade).setScale(1,BigDecimal.RoundingMode.Half UP).toDouble
+     (grade)
+ }   
+  def letterGradeCalculator(grade:Double):String={
+      var letterGrade=""
+      if(grade>=93){
+          letterGrade="A"
+      }
+      else if(grade>=90){
+          letterGrade="A-"
+      
+      }
+      else if(grade>=86){
+          letterGrade="B+"
+      
+      }     
+      else if(grade>=83){
+          letterGrade="B"
+      
+      }     
+      else if(grade>=80){
+          letterGrade="B-"
+      
+      }     
+      else if(grade>=76){
+          letterGrade="C+"
+      
+      }     
+      else if(grade>=73){
+          letterGrade="C"
+      
+      }     
+      else if(grade>=70){
+          letterGrade="C-"
+      
+      }     
+      else if(grade>=66){
+          letterGrade="D+"
+      
+      }     
+      else if(grade>=63){
+          letterGrade="D"
+      
+      }     
+      else if(grade>=60){
+          letterGrade="D-"
+      
+      }     
+      else{ 
+          letterGrade="F"
+      }
+   (letterGrade)
+  }    
+}   
